db2b58542b79ec82edfddcb13b5347c785fab6bf
sneh-01 <122522055+sneh-01@users.noreply.github.com>
2024-04-18 11:12:05 +0530

    array

--- array/KadnessAlgo.java
+++ array/KadnessAlgo.java
@@ -1,9 +1,24 @@
 public class KadnessAlgo {
     public static void kadness(int arr[]){
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            cs = cs + arr[i];
+            if (cs < 0){
+                cs = 0;
+            }
+            ms = Math.max(ms,cs);
+
+        }
+
+        System.out.println(ms);
+
 
     }
     public static void main(String[] args) {
         
-        int arr[] = {1,2,3,4,5,6,};
+        int arr[] = {-2 , -4 , 6 , 0  -1 , 3};
+        kadness(arr);
     }
 }
