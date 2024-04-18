f43271860b59e1816006a4114fc8c16b89cf745e
sneh-01 <122522055+sneh-01@users.noreply.github.com>
2024-04-18 14:38:03 +0530

    sorting

---
+++ string/sorting/BubbleSort.java
@@ -0,0 +1,15 @@
+package sorting;
+
+public class BubbleSort {
+
+    public static void sorting(int arr[]){
+
+        for (int i =0 ; i < arr.length ; i++) {
+
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {2,7,5,4,9};
+    }
+}
