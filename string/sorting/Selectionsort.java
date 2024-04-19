038c679858c631b00600cdf19718c614b6613115
sneh-01 <122522055+sneh-01@users.noreply.github.com>
2024-04-19 16:34:57 +0530

    2d array

---
+++ string/sorting/Selectionsort.java
@@ -0,0 +1,12 @@
+package sorting;
+
+public class Selectionsort {
+
+    public static void sorting(int arr[]){
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {5 , 2, 1 ,4, 3}
+        
+    }
+}
