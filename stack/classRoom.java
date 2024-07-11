e1f65de26523bfb1ff1b4866f28ef0bbf5941ee3
sneh-01 <122522055+sneh-01@users.noreply.github.com>
2024-07-11 16:39:38 +0530

    committed

---
+++ stack/classRoom.java
@@ -0,0 +1,24 @@
+package stack;
+import java.util.*;
+
+public class classRoom {
+
+    public static void MaxArea(String arg){
+        int maxArea = 0;
+        int nsl[] = new int[arr.length];
+        int nsr[] = new int[arr.length];
+        // next smaller right 
+
+
+        // next smaller left
+
+        // current area
+
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2,1,5,6,2,3};
+    }
+    
+}
